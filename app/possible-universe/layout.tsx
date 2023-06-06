0b75cd411352285c5c07caf7343d5f8aaab37600
minumsa <icycyi@naver.com>
2023-06-06 14:04:33 +0900

    metadata 수정

--- app/possible-universe/layout.tsx
+++ app/possible-universe/layout.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import RandomSentenceGenerator from "./RandomSentenceGenerator";
 
+export const metadata = {
+  title: "가능세계: 퀴어 문장 생성기",
+  description:
+    "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
+  openGraph: {
+    title: "가능세계: 퀴어 문장 생성기",
+    description:
+      "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
+  },
+};
+
 type Props = {
   children: React.ReactNode;
 };
