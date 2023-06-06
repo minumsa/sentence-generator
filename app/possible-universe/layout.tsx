6af26e1458f21b2c09b4b5ff6f25a8e713574f17
minumsa <icycyi@naver.com>
2023-06-06 18:56:12 +0900

    twitter description 추가

--- app/possible-universe/layout.tsx
+++ app/possible-universe/layout.tsx
@@ -11,12 +11,12 @@ export const metadata = {
     siteName: "divdivdiv",
     title: "가능세계 — 퀴어 문장 생성기",
     type: "website",
-    // description:
-    //   "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
   },
   twitter: {
     card: "summary_large_image",
     creator: "@dev_carver",
+    description:
+      "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
     images: ["https://divdivdiv.com/pu-thumbnail-1800x945.png"],
     title: "가능세계 — 퀴어 문장 생성기",
   },
