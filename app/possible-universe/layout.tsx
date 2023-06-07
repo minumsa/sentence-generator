485c4d1adf8de2992f9b9e0636de02d300e95b4e
유일 <83910706+minumsa@users.noreply.github.com>
2023-06-07 13:36:55 +0900

    가능세계 문구 삭제

--- app/possible-universe/layout.tsx
+++ app/possible-universe/layout.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import RandomSentenceGenerator from "./RandomSentenceGenerator";
 
 export const metadata = {
-  title: "가능세계 — 퀴어 문장 생성기",
+  title: "퀴어 문장 생성기",
   // description:
   //   "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
   openGraph: {
     images: [{ url: "/pu-thumbnail-1800x945.png", width: 1200, height: 630 }],
     locale: "ko_KR",
     siteName: "divdivdiv",
-    title: "가능세계 — 퀴어 문장 생성기",
+    title: "퀴어 문장 생성기",
     type: "website",
   },
   twitter: {
@@ -18,7 +18,7 @@ export const metadata = {
     description:
       "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
     images: ["https://divdivdiv.com/pu-thumbnail-1800x945.png"],
-    title: "가능세계 — 퀴어 문장 생성기",
+    title: "퀴어 문장 생성기",
   },
 };
 
