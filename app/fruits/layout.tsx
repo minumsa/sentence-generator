1c3a964c59aa22a1a45ae7bfa08759af2438527a
minumsa <icycyi@naver.com>
2023-06-09 18:05:27 +0900

    과일 생성기 썸네일 변경

--- app/fruits/layout.tsx
+++ app/fruits/layout.tsx
@@ -4,7 +4,7 @@ import Fruits from "./Fruits";
 export const metadata = {
   title: "과일 생성기",
   openGraph: {
-    images: [{ url: "/pu-thumbnail-1800x945.png", width: 1800, height: 945 }],
+    images: [{ url: "/fruits-thumbnail.png", width: 1800, height: 945 }],
     locale: "ko_KR",
     siteName: "divdivdiv",
     title: "과일 생성기",
@@ -14,7 +14,7 @@ export const metadata = {
     card: "summary_large_image",
     creator: "@dev_carver",
     description: "화면의 특정 위치에 랜덤하게 과일이 생성된다.",
-    images: ["https://divdivdiv.com/pu-thumbnail-1800x945.png"],
+    images: ["https://divdivdiv.com/fruits-thumbnail.png"],
     title: "과일 생성기",
   },
 };
