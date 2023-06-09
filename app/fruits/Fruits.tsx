bba5d48b136539befb4275c143926c09809ee4ee
minumsa <icycyi@naver.com>
2023-06-09 20:59:36 +0900

    터치 이벤트 추가

--- app/fruits/Fruits.tsx
+++ app/fruits/Fruits.tsx
@@ -31,13 +31,9 @@ const Fruits = () => {
       const fruit = document.createElement("div");
       const randomFruit =
         fruitsArr[Math.floor(Math.random() * fruitsArr.length)];
-      const fruitKey = Date.now().toString(); // 현재 시간을 이용하여 고유한 키 값을 생성한다
       fruit.innerHTML = randomFruit;
       fruit.style.left = `${Math.random() * 100}%`; // 과일이 왼쪽에서부터 어떤 위치에서 내려올지 랜덤하게 지정한다
-      fruit.setAttribute("key", fruitKey); // 고유한 키 값을 설정한다
       container?.appendChild(fruit);
-
-      console.log(fruit);
 
       fruit.style.pointerEvents = "auto"; // 이벤트를 활성화한다
 
@@ -45,13 +41,23 @@ const Fruits = () => {
         fruit.style.cursor = "pointer";
       });
 
-      const clickHandler = () => {
-        setCount(prevCount => prevCount + 1);
-        fruit.removeEventListener("click", clickHandler);
-        fruit.remove();
-      };
+      if (window.outerWidth < 450) {
+        const clickHandler = () => {
+          setCount(prevCount => prevCount + 1);
+          fruit.removeEventListener("touchstart", clickHandler);
+          fruit.remove();
+        };
 
-      fruit.addEventListener("click", clickHandler);
+        fruit.addEventListener("touchstart", clickHandler);
+      } else {
+        const clickHandler = () => {
+          setCount(prevCount => prevCount + 1);
+          fruit.removeEventListener("click", clickHandler);
+          fruit.remove();
+        };
+
+        fruit.addEventListener("click", clickHandler);
+      }
 
       setTimeout(() => {
         fruit.remove();
@@ -86,7 +92,7 @@ const Fruits = () => {
           backgroundPosition: `0 0, 0 ${checkerWidth}px, ${checkerWidth}px -${checkerWidth}px, -${checkerWidth}px 0px`,
         }}
       >
-        <div className="fruit-count">🍋 X {count}</div>
+        <div className="fruit-count">🍋 {count}</div>
         <div id="fruit-container" className="falling-fruits"></div>
       </div>
     </>
