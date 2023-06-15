f8ea6abe5cf95e7f26e36217f5728f348846be54
유일 <83910706+minumsa@users.noreply.github.com>
2023-06-15 16:16:10 +0900

    테스트 문제 5번까지 완성

--- app/cinephile-test/Test5.tsx
+++ app/cinephile-test/Test5.tsx
@@ -12,19 +12,78 @@ export default function Test5() {
           <span style={{ textDecoration: "underline" }}>아닌 </span>
           <span>것을 선택하시오.</span>
         </div>
-        <div>
-          <div style={{ marginBottom: "1px" }}>(1) </div>
-          <Image
-            src="https://i.namu.wiki/i/vmosz3KHdVQ4Pe66JbcWIgpj_YVCdju0CDPK1tMO_1Pznq7v4GPIqVBE8SYfoa0BrSuEK09LJLRS9Zkk02TNVpoF7xNpbM12tmfVDn2OLbqg9kqrCX840Jsv8qRw5U-srlPcCDdjXF2FzfKgz42F_A.svg"
-            alt="Cannes"
-            width="150"
-            height="75"
-            style={{ marginBottom: "8px" }}
-          />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <div>(1) </div>
+          <div>
+            {" "}
+            <Image
+              src="https://i.namu.wiki/i/vmosz3KHdVQ4Pe66JbcWIgpj_YVCdju0CDPK1tMO_1Pznq7v4GPIqVBE8SYfoa0BrSuEK09LJLRS9Zkk02TNVpoF7xNpbM12tmfVDn2OLbqg9kqrCX840Jsv8qRw5U-srlPcCDdjXF2FzfKgz42F_A.svg"
+              alt="Cannes"
+              width="150"
+              height="75"
+              style={{ marginBottom: "8px", marginTop: "10px" }}
+            />
+          </div>
+          <div style={{ marginLeft: "50px" }}>(2) </div>
+          <div>
+            {" "}
+            <Image
+              src=" https://i.namu.wiki/i/DQ5hBTG06AnGXl7LQHWlV3lMcZLt16azSj9K1dOzgBy-gWFq73ZJc5i8gwrirngmcah6Y3_UUGRYbmAV8VsML6pNf07KzFUxqkJX1fGUXzPziIYjL8FSK8pSXtY9HdBglYtf0LgY4-kc_qmcoET5nw.svg"
+              alt="Venice"
+              width="150"
+              height="80"
+              style={{
+                marginLeft: "22px",
+                marginBottom: "8px",
+                marginTop: "10px",
+              }}
+            />
+          </div>
         </div>
-        <div>(2) 황야의 무법자</div>
-        <div>(3) 북북서로 진로를 돌려라</div>
-        <div>(4) 네 멋대로 해라</div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <div>(3) </div>
+          <div style={{ width: "150px" }}>
+            {" "}
+            <Image
+              src="/iffr.jpeg"
+              alt="Oscars"
+              width="125"
+              height="110"
+              style={{
+                marginLeft: "10px",
+                marginBottom: "8px",
+                marginTop: "10px",
+              }}
+            />
+          </div>
+          <div style={{ marginLeft: "50px" }}>(4) </div>
+          <div>
+            {" "}
+            <Image
+              src="https://i.namu.wiki/i//tvU9Fd43KcGcWsGVAGkPJvCacjzvcf00nQKrJmqHo6CoZt5pqCwz77izwcjKkmO_O1479eAuYXOFfo7VROsCyJMY5aVx5kchHvEvqYTrnfcNbbfg8O_zayC9GDUZqv9D-5bvZ-svlFn6YBo1q1imYg.svg"
+              alt="Venice"
+              width="150"
+              height="88"
+              style={{
+                marginLeft: "22px",
+                marginBottom: "8px",
+                marginTop: "10px",
+              }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
