815e093f711a913451e7a0faf68850718c33673e
minumsa <icycyi@naver.com>
2023-06-15 14:00:32 +0900

    이전 문제, 다음 문제 버튼 만들기

--- app/cinephile-test/Cinephile.tsx
+++ app/cinephile-test/Cinephile.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
+import Test1 from "./Test1";
 
 export default function Cinephile() {
+  const [testNumber, setTestNumber] = useState<number>(1);
+
+  function handleTest() {
+    if (testNumber === 1) {
+      return (
+        <>
+          <Test1 />
+        </>
+      );
+    }
+  }
+
   return (
     <div className="cine-container">
       <div className="cine-flex-container">
         <div className="cine-nav-container">nav</div>
-        <div className="cine-content-container">content</div>
-        <div className="cine-footer-container">footer</div>
+        <div className="cine-content-container">{handleTest()}</div>
+        <div className="cine-footer-container">
+          <div className="cine-prev-button-flex">
+            <div className="cine-prev-button">이전 문제</div>
+          </div>
+          <div className="cine-next-button-flex">
+            <div className="cine-next-button">다음 문제</div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ app/cinephile-test/Test1.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export default function Test1() {
+  return (
+    <>
+      <div>something</div>
+    </>
+  );
+}
