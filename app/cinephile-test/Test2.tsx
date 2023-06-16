f34d2cc20cfd14bc0f753dc2abfdfae1c00e091a
유일 <83910706+minumsa@users.noreply.github.com>
2023-06-16 15:56:36 +0900

    Test2, 3 CSS 변경

--- app/cinephile-test/Test2.tsx
+++ app/cinephile-test/Test2.tsx
@@ -7,19 +7,28 @@ interface TestProps {
   setScore: React.Dispatch<React.SetStateAction<number>>;
 }
 
+interface AnswerStyle {
+  color: string;
+  backgroundColor: string;
+}
+
 export default function Test2({ score, setScore }: TestProps) {
   const [mark1, setMark1] = useState<React.CSSProperties>({});
   const [mark2, setMark2] = useState<React.CSSProperties>({});
   const [mark3, setMark3] = useState<React.CSSProperties>({});
   const [mark4, setMark4] = useState<React.CSSProperties>({});
   const [copiedScore, setCopiedScore] = useState<number>(0);
+  const [answerStyle, setAnswerStyle] = useState<AnswerStyle>({
+    color: "white",
+    backgroundColor: "#0e1111",
+  });
 
   useEffect(() => {
     setCopiedScore(score);
   }, []);
 
   function clickAnswer1() {
-    setMark1({ border: "2.5px dashed #0e1111" });
+    setMark1(answerStyle);
     setMark2({});
     setMark3({});
     setMark4({});
@@ -28,7 +37,7 @@ export default function Test2({ score, setScore }: TestProps) {
 
   function clickAnswer2() {
     setMark1({});
-    setMark2({ border: "2.5px dashed #0e1111" });
+    setMark2(answerStyle);
     setMark3({});
     setMark4({});
     setScore(copiedScore);
@@ -37,7 +46,7 @@ export default function Test2({ score, setScore }: TestProps) {
   function clickAnswer3() {
     setMark1({});
     setMark2({});
-    setMark3({ border: "2.5px dashed #0e1111" });
+    setMark3(answerStyle);
     setMark4({});
     setScore((score: number) => copiedScore + 4);
   }
@@ -46,7 +55,7 @@ export default function Test2({ score, setScore }: TestProps) {
     setMark1({});
     setMark2({});
     setMark3({});
-    setMark4({ border: "2.5px dashed #0e1111" });
+    setMark4(answerStyle);
     setScore(copiedScore);
   }
 
@@ -57,16 +66,16 @@ export default function Test2({ score, setScore }: TestProps) {
         <span>2. 다음 중 소설 원작이 </span>
         <span style={{ textDecoration: "underline" }}>아닌</span>
         <span> 영화는?</span>
-        <div style={mark1} onClick={clickAnswer1}>
+        <div className="cine-answer" style={mark1} onClick={clickAnswer1}>
           (1) 케빈에 대하여(2011, 린 램지){" "}
         </div>
-        <div style={mark2} onClick={clickAnswer2}>
+        <div className="cine-answer" style={mark2} onClick={clickAnswer2}>
           (2) 콜 미 바이 유어 네임(2017, 루카 구아다니노)
         </div>
-        <div style={mark3} onClick={clickAnswer3}>
+        <div className="cine-answer" style={mark3} onClick={clickAnswer3}>
           (3) 지구 최후의 밤(2018, 비간)
         </div>
-        <div style={mark4} onClick={clickAnswer4}>
+        <div className="cine-answer" style={mark4} onClick={clickAnswer4}>
           (4) 서부 전선 이상 없다(2022, 에드워드 버거)
         </div>
       </div>
--- app/cinephile-test/Test3.tsx
+++ app/cinephile-test/Test3.tsx
@@ -7,19 +7,28 @@ interface TestProps {
   setScore: React.Dispatch<React.SetStateAction<number>>;
 }
 
+interface AnswerStyle {
+  color: string;
+  backgroundColor: string;
+}
+
 export default function Test3({ score, setScore }: TestProps) {
   const [mark1, setMark1] = useState<React.CSSProperties>({});
   const [mark2, setMark2] = useState<React.CSSProperties>({});
   const [mark3, setMark3] = useState<React.CSSProperties>({});
   const [mark4, setMark4] = useState<React.CSSProperties>({});
   const [copiedScore, setCopiedScore] = useState<number>(0);
+  const [answerStyle, setAnswerStyle] = useState<AnswerStyle>({
+    color: "white",
+    backgroundColor: "#0e1111",
+  });
 
   useEffect(() => {
     setCopiedScore(score);
   }, []);
 
   function clickAnswer1() {
-    setMark1({ border: "2.5px dashed #0e1111" });
+    setMark1(answerStyle);
     setMark2({});
     setMark3({});
     setMark4({});
@@ -28,7 +37,7 @@ export default function Test3({ score, setScore }: TestProps) {
 
   function clickAnswer2() {
     setMark1({});
-    setMark2({ border: "2.5px dashed #0e1111" });
+    setMark2(answerStyle);
     setMark3({});
     setMark4({});
     setScore((score: number) => copiedScore + 4);
@@ -37,7 +46,7 @@ export default function Test3({ score, setScore }: TestProps) {
   function clickAnswer3() {
     setMark1({});
     setMark2({});
-    setMark3({ border: "2.5px dashed #0e1111" });
+    setMark3(answerStyle);
     setMark4({});
     setScore(copiedScore);
   }
@@ -46,7 +55,7 @@ export default function Test3({ score, setScore }: TestProps) {
     setMark1({});
     setMark2({});
     setMark3({});
-    setMark4({ border: "2.5px dashed #0e1111" });
+    setMark4(answerStyle);
     setScore(copiedScore);
   }
 
@@ -54,16 +63,16 @@ export default function Test3({ score, setScore }: TestProps) {
     <>
       <div className="cine-test-format">
         <div>3. 다음 중 러닝타임이 가장 긴 영화는?</div>
-        <div style={mark1} onClick={clickAnswer1}>
+        <div className="cine-answer" style={mark1} onClick={clickAnswer1}>
           (1) 잠(1964, 앤디 워홀)
         </div>
-        <div style={mark2} onClick={clickAnswer2}>
+        <div className="cine-answer" style={mark2} onClick={clickAnswer2}>
           (2) 사탄 탱고(1994, 터르 벨러)
         </div>
-        <div style={mark3} onClick={clickAnswer3}>
+        <div className="cine-answer" style={mark3} onClick={clickAnswer3}>
           (3) 반지의 제왕: 왕의 귀환(2003, 피터 잭슨)
         </div>
-        <div style={mark4} onClick={clickAnswer4}>
+        <div className="cine-answer" style={mark4} onClick={clickAnswer4}>
           (4) 해피 아워(2015, 하마구치 류스케)
         </div>
       </div>
