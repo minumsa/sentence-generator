ad7aa471a772e4d96d2428b9a80612f1a44f37ff
유일 <83910706+minumsa@users.noreply.github.com>
2023-06-19 18:52:34 +0900

    test

--- app/cinephile-test/Test0.tsx
+++ app/cinephile-test/Test0.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
 export default function Test0() {
   return (
     <>
-      <div
+      {/* <div
         style={{ width: "100%", height: "470px", border: "1.5px solid black" }}
-      >
-        <Image
-          src="/cine-img-0.png"
-          alt="cine-img-0"
-          width={window.innerWidth > 450 ? "550" : "350"}
-          height={window.innerWidth > 450 ? "470" : "200"}
-          style={{}}
-        />
-      </div>
+      ></div> */}
     </>
   );
 }
