8710027d41ae3ebfb0a6c86985d31bc6a74ae21d
유일 <83910706+minumsa@users.noreply.github.com>
2023-06-26 17:50:59 +0900

    드래거블 기능 추가

--- app/Index.tsx
+++ app/Index.tsx
@@ -1,83 +1,74 @@
 "use client";
 
-import Image from "next/image";
-import React, { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 const Index: React.FC = () => {
   const [folderWidth, setFolderWidth] = useState<number>(800);
   const [folderHeight, setFolderHeight] = useState<number>(648);
-
-  const folderRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
   const [isDragging, setIsDragging] = useState(false);
-  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
 
-  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-    setIsDragging(true);
-    const { left, top } = folderRef.current!.getBoundingClientRect();
-    const offsetX = event.clientX - left;
-    const offsetY = event.clientY - top;
-    setDragOffset({ x: offsetX, y: offsetY });
+  const clickIconHandler = (path: string) => {
+    router.push(path);
   };
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!isDragging) return;
-    const x = event.clientX - dragOffset.x;
-    const y = event.clientY - dragOffset.y;
-    folderRef.current!.style.transform = `translate(${x}px, ${y}px) translateZ(0)`;
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
+  const IconContainer: React.FC<{
+    path: string;
+    projectName: string;
+    projectDescription: string;
+    className: string;
+  }> = ({ path, projectName, projectDescription, className }) => (
+    <Draggable>
+      <div className={className} onDoubleClick={() => clickIconHandler(path)}>
+        <div
+          className="index-icon-image"
+          style={{
+            color: "white",
+            cursor: "move",
+            backgroundImage: `url(folder.png)`,
+            backgroundSize: "100%",
+            width: folderWidth / 9,
+            height: folderHeight / 9,
+          }}
+        ></div>
+        <div className="index-icon-text">
+          <div>{projectName}</div>
+          <div>({projectDescription})</div>
+        </div>
+      </div>
+    </Draggable>
+  );
 
   return (
     <div
       style={{ width: "100vw", height: "100vh", backgroundColor: "#0E1111" }}
     >
-      {/* <a href="/sheep-pomodoro"> */}
-      <div
-        className="folder"
-        style={{
-          position: "absolute",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          cursor: "move",
-          zIndex: 100,
-          pointerEvents: "auto",
-          transitionDuration: "initial",
-        }}
-        ref={folderRef}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-      >
-        <div>
-          <Image
-            src="/folder.png"
-            alt="folder"
-            width={folderWidth / 8}
-            height={folderHeight / 8}
-            priority
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontSize: "15px",
-            fontWeight: "bold",
-            color: "#ffffff",
-            lineHeight: "1.3",
-            textShadow: "1px 1px 3px #000;",
-          }}
-        >
-          <div>Project 1</div>
-          <div>(Sheep Pomodoro)</div>
-        </div>
-      </div>
-      {/* </a> */}
+      <IconContainer
+        path="/sheep-pomodoro"
+        projectName="Project 1"
+        projectDescription="Sheep Pomodoro"
+        className="index-icon-container"
+      />
+      <IconContainer
+        path="/cinephile-test"
+        projectName="Project 2"
+        projectDescription="Cinephile Test"
+        className="index-icon-container-2"
+      />
+      <IconContainer
+        path="/possible-universe"
+        projectName="Project 3"
+        projectDescription="Possible Universe"
+        className="index-icon-container-3"
+      />
+      <IconContainer
+        path="/fruits"
+        projectName="Project 4"
+        projectDescription="Fruits"
+        className="index-icon-container-4"
+      />
     </div>
   );
 };
