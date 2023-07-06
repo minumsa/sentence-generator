5a7f845ab44f7a5022b0a881db32c2487a7b58a5
유일 <83910706+minumsa@users.noreply.github.com>
2023-07-06 17:26:55 +0900

    썸네일 오타 수정

--- app/lunch/layout.tsx
+++ app/lunch/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   // description:
   //   "데이터에 입력된 주어, 동사, 목적어가 무작위하게 조합되며 다양한 퀴어 문장을 생성한다. 모든 문장은 미래형이며 아직은 완성되지 못한 가능세계를 암시한다.",
   openGraph: {
-    images: [{ url: "/lunch-thumbnail.webp", width: 1800, height: 945 }],
+    images: [{ url: "/lunch_thumbnail.webp", width: 1800, height: 945 }],
     locale: "ko_KR",
     siteName: "divdivdiv",
     title: "오늘의 점심",
@@ -16,7 +16,7 @@ export const metadata = {
     card: "summary_large_image",
     creator: "@dev_carver",
     description: "오늘의 점심을 추천해드립니다.",
-    images: ["https://divdivdiv.com/lunch-thumbnail"],
+    images: ["https://divdivdiv.com/lunch_thumbnail"],
     title: "오늘의 점심",
   },
 };
