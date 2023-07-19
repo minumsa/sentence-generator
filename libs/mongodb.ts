f6b9ed3edc797755e901b53fa9b581badd0ba214
유일 <83910706+minumsa@users.noreply.github.com>
2023-07-19 17:35:37 +0900

    dotenv 테스트

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
+    require("dotenv").config();
     await mongoose.connect(process.env.MONGODB_URI!);
 
     console.log("Connected to MongoDB");
