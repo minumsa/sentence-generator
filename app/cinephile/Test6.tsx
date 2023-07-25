9e48684f613f2560dece63023a7dce0ec8b06d7c
minumsa <icycyi@naver.com>
2023-07-26 00:12:54 +0900

    quiz.json 만들기 시작

--- app/cinephile/Test6.tsx
+++ app/cinephile/Test6.tsx
@@ -33,7 +33,6 @@ export default function Test6({ score, setScore }: TestProps) {
 
   function clickAnswer(answerIndex: number) {
     if (answers[answerIndex].backgroundColor === answerStyle.backgroundColor) {
-      // 클릭한 답변을 다시 클릭하면 초기화
       setAnswers(prevAnswers => {
         const updatedAnswers = [...prevAnswers];
         updatedAnswers[answerIndex] = {};
@@ -51,15 +50,7 @@ export default function Test6({ score, setScore }: TestProps) {
 
   return (
     <>
-      <div
-        className={styles["cine-test-format"]}
-        style={
-          {
-            // marginTop: window.innerWidth > 450 ? "15px" : "0",
-            // marginBottom: window.innerWidth > 450 ? "20px" : "0",
-          }
-        }
-      >
+      <div className={styles["cine-test-format"]}>
         <div className={styles["cine-quiz"]}>
           {`6. 다음은 영화 <샤이닝>(1980, 스탠리 큐브릭)의 한 장면이다. 사진 속 인물의 대사로 가장 적절한 것은?`}
         </div>
