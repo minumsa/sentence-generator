84fd8c45970160521ff339502263cf6dd3c71ef0
유일 <83910706+minumsa@users.noreply.github.com>
2023-07-28 14:27:38 +0900

    주관식 문제 로직 만들기

--- app/cinephile/Question.tsx
+++ app/cinephile/Question.tsx
@@ -2,72 +2,124 @@ import { useEffect, useState } from "react";
 import styles from "./cine.module.css";
 import { data } from "./data";
 import React from "react";
+import Image from "next/image";
 
 interface QuestionProps {
   page: number;
   score: number;
   setScore: React.Dispatch<React.SetStateAction<number>>;
+  setTotalScore: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export function Question({ page, score, setScore }: QuestionProps) {
+export function Question({ page, score, setScore, setTotalScore }: QuestionProps) {
   // TODO: useEffect 안 쓰고 useState(score)하면 될듯!
-  const [totalScore, setTotalScore] = useState<number>(0);
-  const [selectedAnswer, setSelectedAnswer] = useState<number>();
-  const negative: string[] = ["아닌", "않은", "않는"];
+
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const negativeWord: string[] = ["아닌", "않은", "않는"];
+  const [isNagative, setIsNagative] = useState<boolean>(false);
 
   useEffect(() => {
     setTotalScore(prevScore => prevScore + score);
+
+    setSelectedAnswer(null);
+    setIsNagative(false);
+
+    negativeWord.map(word => {
+      if (data[page - 1].question.includes(word)) setIsNagative(true);
+    });
   }, [page]);
 
   function handleClick(answer: number) {
     setSelectedAnswer(answer);
 
-    if (data[page].answer === answer && answer !== selectedAnswer) {
+    if (data[page - 1].answer === answer && answer !== selectedAnswer) {
       setScore(4);
-    } else if (data[page].answer !== answer) {
+    } else if (data[page - 1].answer !== answer) {
       setScore(0);
     }
   }
 
+  const Options = () => {
+    return data[page - 1].options?.map((option, index) => {
+      return (
+        <React.Fragment key={index}>
+          <div
+            className={styles["options"]}
+            style={{
+              backgroundColor: index === selectedAnswer ? "#000000" : undefined,
+              color: index === selectedAnswer ? "#ffffff" : undefined,
+            }}
+            onClick={() => handleClick(index)}
+          >
+            {`${index + 1}) ${option}`}
+          </div>
+        </React.Fragment>
+      );
+    });
+  };
+
   return (
     <div className={styles["question-container"]}>
       <div className={styles["question"]}>
-        {`${[page + 1]}. `}
-        {negative.map((word, index) => {
-          if (data[page].question.includes(word)) {
-            const text = data[page].question.split(word);
-            return (
-              <React.Fragment key={index}>
-                <span>{text[0]}</span>
-                <span style={{ textDecoration: "underline" }}>{word}</span>
-                <span>{text[1]}</span>
-              </React.Fragment>
-            );
-          } else {
-            return null;
-          }
-        })}
+        {`${[page]}. `}
+        {isNagative
+          ? negativeWord.map((word, index) => {
+              if (data[page - 1].question.includes(word)) {
+                const text = data[page - 1].question.split(word);
+                return (
+                  <React.Fragment key={index}>
+                    <span>{text[0]}</span>
+                    <span style={{ textDecoration: "underline" }}>{word}</span>
+                    <span>{text[1]}</span>
+                  </React.Fragment>
+                );
+              } else {
+                return null;
+              }
+            })
+          : data[page - 1].question}
       </div>
-      {data[page].type === "multiple-choice" ? (
-        data[page].options?.map((option, index) => {
-          return (
-            <div
-              className={styles["options"]}
-              style={{
-                backgroundColor: index === selectedAnswer ? "#000000" : undefined,
-                color: index === selectedAnswer ? "#ffffff" : undefined,
-              }}
-              key={index}
-              onClick={() => handleClick(index)}
-            >
-              {`${index + 1}) ${option}`}
+      {data[page - 1].type === "multiple-choice" ? (
+        data[page - 1].type2 === "image" ? (
+          <React.Fragment>
+            <div className={styles["image-container"]}>
+              <Image
+                className={styles["image"]}
+                src={`/cinephile/${data[page - 1].title}.webp`}
+                alt={`${data[page - 1].title}`}
+                width={window.innerWidth > 450 ? "280" : "180"}
+                height={window.innerWidth > 450 ? "190" : "120"}
+              />
             </div>
-          );
-        })
-      ) : data[page].type === "short-answer" ? (
+            <Options />
+          </React.Fragment>
+        ) : data[page - 1].movie === "chungking-express" ? (
+          <React.Fragment>
+            <div className={styles["chungking-express"]}>📞 🍍 🕒 😎</div>
+            <div className={styles["chungking-express"]}>👮‍♂️ 💌 🔑 🛫</div>
+            <Options />
+          </React.Fragment>
+        ) : (
+          <Options />
+        )
+      ) : data[page - 1].type === "short-answer" ? (
         <div className={styles["short-answer-container"]}>
-          <input className={styles["short-answer-input"]} onChange={e => setMark(e.target.value)} />
-          {` ${data[page].paragraph}`}
+          {data[page - 1].paragraph
+            ?.split(String(data[page - 1].answer))
+            // .splice(0, 0, String(data[page - 1].answer))
+            .map((text, index) => {
+              return <React.Fragment key={index}>yes</React.Fragment>;
+              // return text === String(data[page - 1].answer) ? (
+              //   <input
+              //     className={styles["short-answer-input"]}
+              //     onChange={e => setMark(e.target.value)}
+              //   ></input>
+              // ) : (
+              //   <React.Fragment key={index}>
+              //     <span>{text}</span>
+              //   </React.Fragment>
+              // );
+            })}
         </div>
       ) : (
         ""
--- app/cinephile/page.tsx
+++ app/cinephile/page.tsx
@@ -1,26 +1,46 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./cine.module.css";
 import { Question } from "./Question";
 
 export default function Page() {
-  const [score, setScore] = useState<number>(0);
-  const [page, setPage] = useState<number>(0);
   const [pageType, setPageType] = useState<"index" | "question" | "result">("index");
+  const [score, setScore] = useState<number>(0);
+  const [totalScore, setTotalScore] = useState<number>(0);
+  const [testPage, setTestPage] = useState<number>(1);
+  const testPageMax = 25;
+  const progressWidth = `${(testPage / (testPageMax - 1)) * 100}%`;
+  const progressPercent = `${Math.floor((testPage / (testPageMax - 1)) * 100)}%`;
 
   const handleButton = () => {
     if (pageType === "index") setPageType("question");
-    if (pageType === "question") setPage(page => page + 1);
+    if (pageType === "question") setTestPage(page => page + 1);
   };
+
+  console.log("totalScore", totalScore);
 
   return (
     <div className={styles["container"]}>
       <div className={styles["content-container"]}>
         <div className={styles["title"]}>시네필 테스트</div>
+        {pageType === "question" ? (
+          <div className={styles["progress-container"]}>
+            <div className={styles["progress-content"]} style={{ width: progressWidth }}>
+              <div className={styles["progress-font"]}>{progressPercent}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
         <div className={styles["content"]}>
           {pageType === "question" ? (
-            <Question page={page} score={score} setScore={setScore} />
+            <Question
+              page={testPage}
+              score={score}
+              setScore={setScore}
+              setTotalScore={setTotalScore}
+            />
           ) : (
             ""
           )}
