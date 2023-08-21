2dba2612d00c745b60cc42da3eced75c1b340051
minumsa <icycyi@naver.com>
2023-08-21 22:15:37 +0900

    로딩화면 테스트

--- app/music/lib/Content.tsx
+++ app/music/lib/Content.tsx
@@ -159,81 +159,85 @@ export default function Content({ pathName, fullPathName }: pageProps) {
             />
           </div>
         )}
-        {sortedData.map((data, index) => {
-          // FIXME: 시간을 나타내주는 유틸리티 함수(formatDuration())를 만들어라.
-          // utils.ts로 빼라.
-          const minutes = Math.floor(data.duration / 60);
-          const hours = Math.floor(minutes / 60);
+        {data ? (
+          sortedData.map((data, index) => {
+            // FIXME: 시간을 나타내주는 유틸리티 함수(formatDuration())를 만들어라.
+            // utils.ts로 빼라.
+            const minutes = Math.floor(data.duration / 60);
+            const hours = Math.floor(minutes / 60);
 
-          return (
-            <div key={index}>
-              <div className={styles["album-container"]}>
-                <div className={styles["album-information-container"]}>
-                  <div className={styles["album-art"]}>
-                    <a className={styles["link"]} href={data.link} target="_blank">
-                      <Image src={data.imgUrl} alt={data.album} fill={true} />
-                    </a>
-                  </div>
-                  <div className={` ${styles["album-information"]}`}>
-                    <div>
-                      <div className={styles["information"]}>
-                        <div style={{ marginRight: "5px" }}>{data.artist}</div>
-                      </div>
-                      <div className={styles["information"]}>
-                        <a className={styles["link"]} href={data.link} target="_blank">
-                          <div className={styles["album-title"]}>{data.album}</div>
-                        </a>
-                      </div>
+            return (
+              <div key={index}>
+                <div className={styles["album-container"]}>
+                  <div className={styles["album-information-container"]}>
+                    <div className={styles["album-art"]}>
+                      <a className={styles["link"]} href={data.link} target="_blank">
+                        <Image src={data.imgUrl} alt={data.album} fill={true} />
+                      </a>
                     </div>
-                    <div className={styles["information"]}>
-                      <span>{`${data.releaseDate.slice(0, 4)}년 ${Number(
-                        data.releaseDate.slice(5, 7)
-                      )}월, ${data.label}`}</span>
-                    </div>
-                    <div className={styles["information"]}>
-                      {`${data.tracks}곡, `}
-                      {minutes > 60
-                        ? `${hours}시간 ${minutes % 60 > 0 ? `${minutes % 60}분` : ""}`
-                        : `${minutes}분`}
-                    </div>
-                    {fullPathName.includes("admin") && (
-                      <div className={styles["admin-button-container"]}>
-                        <div
-                          className={styles["admin-button"]}
-                          onClick={async () => {
-                            deleteData(data.id);
-                            setData(await fetchData(pathName));
-                          }}
-                        >
-                          삭제
+                    <div className={` ${styles["album-information"]}`}>
+                      <div>
+                        <div className={styles["information"]}>
+                          <div style={{ marginRight: "5px" }}>{data.artist}</div>
                         </div>
-                        <div
-                          className={styles["admin-button"]}
-                          onClick={() => {
-                            router.push(`/music/admin/${data.id}`);
-                          }}
-                        >
-                          수정
+                        <div className={styles["information"]}>
+                          <a className={styles["link"]} href={data.link} target="_blank">
+                            <div className={styles["album-title"]}>{data.album}</div>
+                          </a>
                         </div>
                       </div>
-                    )}
+                      <div className={styles["information"]}>
+                        <span>{`${data.releaseDate.slice(0, 4)}년 ${Number(
+                          data.releaseDate.slice(5, 7)
+                        )}월, ${data.label}`}</span>
+                      </div>
+                      <div className={styles["information"]}>
+                        {`${data.tracks}곡, `}
+                        {minutes > 60
+                          ? `${hours}시간 ${minutes % 60 > 0 ? `${minutes % 60}분` : ""}`
+                          : `${minutes}분`}
+                      </div>
+                      {fullPathName.includes("admin") && (
+                        <div className={styles["admin-button-container"]}>
+                          <div
+                            className={styles["admin-button"]}
+                            onClick={async () => {
+                              deleteData(data.id);
+                              setData(await fetchData(pathName));
+                            }}
+                          >
+                            삭제
+                          </div>
+                          <div
+                            className={styles["admin-button"]}
+                            onClick={() => {
+                              router.push(`/music/admin/${data.id}`);
+                            }}
+                          >
+                            수정
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <div className={styles["text-container"]}>
+                    {/* FIXME: 클릭하면 링크로 연결되는 기능 만들기 */}
+                    {data.text.split("\n").map((text, index) => {
+                      return (
+                        <p key={index} className={styles["paragraph"]}>
+                          {text}
+                        </p>
+                      );
+                    })}
                   </div>
                 </div>
-                <div className={styles["text-container"]}>
-                  {/* FIXME: 클릭하면 링크로 연결되는 기능 만들기 */}
-                  {data.text.split("\n").map((text, index) => {
-                    return (
-                      <p key={index} className={styles["paragraph"]}>
-                        {text}
-                      </p>
-                    );
-                  })}
-                </div>
+                <div className={styles["divider"]} />
               </div>
-              <div className={styles["divider"]} />
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div>데이터를 불러오고 있습니다...</div>
+        )}
       </div>
     </div>
   );
