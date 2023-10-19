85afad1caa449851759553af6872a41028f413c3
유일 <83910706+minumsa@users.noreply.github.com>
2023-10-19 15:57:28 +0900

    문장 생성기 단어 변경

--- app/pride/page.tsx
+++ app/pride/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./pride.module.css";
-import { objects, subjects, verbs } from "./words";
+import { words } from "./words";
 
 // 따로 뺀 거 좋다. 컴포넌트와 관계 없는 로직이란 것을 보여줘서. 이 함수 내에서 컴포넌트 안에 있는 변수를 사용하지 않는다는 걸 보장해줘서.
 const getRandomItemFromArray = (array: string[]): string => {
@@ -14,15 +14,13 @@ const generateRandomColor = (): string => {
 };
 
 export default function RandomSentenceGenerator() {
-  const [randomSubject, setRandomSubject] = useState<string>("Pride");
-  const [randomObject, setRandomObject] = useState<string>("Month");
-  const [randomVerb, setRandomVerb] = useState<string>("2023");
+  const [randomWord1, setRandomWord1] = useState<string>(words[0]);
+  const [randomWord2, setRandomWord2] = useState<string>(words[9]);
   const [isRunning, setIsRunning] = useState<boolean>(true);
 
   const generateRandomSentence = () => {
-    setRandomSubject(getRandomItemFromArray(subjects));
-    setRandomObject(getRandomItemFromArray(objects));
-    setRandomVerb(getRandomItemFromArray(verbs));
+    setRandomWord1(getRandomItemFromArray(words));
+    setRandomWord2(getRandomItemFromArray(words));
   };
 
   // FIXME: isRunning의 변수 이름이 조금 이상. false일 때도 활성화되기 때문에.
@@ -40,62 +38,33 @@ export default function RandomSentenceGenerator() {
   };
 
   return (
-    // FIXME: 모바일 div 없애고 하나로 합치기. 미디어 쿼리 이용.
     <div className={styles["container"]}>
       <div className={styles["sentence-container"]}>
         <div className={styles["sentence"]} onClick={handleClick}>
           <span
-            className={styles["subject"]}
+            className={styles["word1"]}
             style={{
               color: generateRandomColor(),
             }}
           >
-            {randomSubject}{" "}
+            {randomWord1}{" "}
           </span>
           <span
-            className={styles["object"]}
+            className={styles["word3"]}
             style={{
               color: generateRandomColor(),
             }}
           >
-            {randomObject}{" "}
+            같은{" "}
           </span>
           <span
-            className={styles["verb"]}
+            className={styles["word2"]}
             style={{
               color: generateRandomColor(),
             }}
           >
-            {randomVerb}
+            {randomWord2}
           </span>
-        </div>
-        <div className={styles["mobile-container"]} onClick={handleClick}>
-          <div className={styles["mobile-sentence"]}>
-            <div
-              className={styles["mobile-subject"]}
-              style={{
-                color: generateRandomColor(),
-              }}
-            >
-              {randomSubject}{" "}
-            </div>
-            <div
-              className={styles["mobile-object"]}
-              style={{
-                color: generateRandomColor(),
-              }}
-            >
-              {randomObject}{" "}
-            </div>
-          </div>
-          <div
-            className={styles["mobile-verb"]}
-            style={{
-              color: generateRandomColor(),
-            }}
-          >
-            {randomVerb}
-          </div>
         </div>
       </div>
     </div>
