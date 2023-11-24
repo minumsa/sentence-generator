3ceaaa30f85737f964346cc7537712c2e87d9188
유일 <83910706+minumsa@users.noreply.github.com>
2023-11-24 11:31:58 +0900

    nossr to Snow

--- app/music/components/Category.tsx
+++ app/music/components/Category.tsx
@@ -24,6 +24,7 @@ export const Category = ({ pathName, fullPathName }: CategoryProps) => {
         className={styles["category"]}
         onClick={() => {
           setBoardIsVisible(!boardIsVisible);
+          router.push("/music");
         }}
         style={pathName === "" ? activeStyle : {}}
       >
--- app/music/components/Snow.tsx
+++ app/music/components/Snow.tsx
@@ -1,15 +1,18 @@
 import Snowfall from "react-snowfall";
 import { isMobile } from "react-device-detect";
 import styles from "../music.module.css";
+import NoSSR from "@/app/divdivdiv/NoSSR";
 
 export const Snow = () => {
   return (
-    <div className={styles["snowfall-container"]}>
-      <Snowfall
-        snowflakeCount={isMobile ? 90 : 110}
-        speed={isMobile ? [0, 2] : [0, 2.5]}
-        radius={isMobile ? [0.1, 1.2] : undefined}
-      />
-    </div>
+    <NoSSR>
+      <div className={styles["snowfall-container"]}>
+        <Snowfall
+          snowflakeCount={isMobile ? 90 : 110}
+          speed={isMobile ? [0, 2] : [0, 2.5]}
+          radius={isMobile ? [0.1, 1.2] : undefined}
+        />
+      </div>
+    </NoSSR>
   );
 };
