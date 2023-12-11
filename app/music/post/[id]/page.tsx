c8ada28c84bfdd5e50ab4acff0a9a3b5c63688ef
유일 <83910706+minumsa@users.noreply.github.com>
2023-12-11 13:03:39 +0900

    meta head test

--- app/music/post/[id]/page.tsx
+++ app/music/post/[id]/page.tsx
@@ -1,5 +1,4 @@
-// Remove the "use client" directive
-// "use client";
+"use client";
 
 import { AlbumInfo, PageProps, initialAlbumInfo } from "../../modules/data";
 import { Post } from "../../components/Post";
@@ -7,23 +6,39 @@ import { MusicLayout } from "../../components/MusicLayout";
 import { useEffect, useState } from "react";
 import { fetchDataById } from "../../modules/api";
 import { metadata } from "../../layout";
+import Head from "next/head";
 
 export default function Page({ params }: PageProps) {
   const currentId = params.id;
+  const [data, setData] = useState<AlbumInfo>(initialAlbumInfo);
+
+  useEffect(() => {
+    async function getData() {
+      const result = await fetchDataById(currentId);
+      setData(result);
+    }
+    getData();
+  }, []);
 
   return (
-    <MusicLayout>
-      <Post pathName={currentId} />
-    </MusicLayout>
+    <>
+      <Head>
+        <title>{data.album}</title>
+        <meta property="og:title" content={data.album} key="title" />
+      </Head>
+      <MusicLayout>
+        <Post pathName={currentId} />
+      </MusicLayout>
+    </>
   );
 }
 
-export async function generateMetadata({ params }: any) {
-  const id = params.id;
-  const result = await fetchDataById(id);
+// export async function generateMetadata({ params }: any) {
+//   const id = params.id;
+//   const result = await fetchDataById(id);
 
-  return {
-    title: result.album,
-    description: result.text,
-  };
-}
+//   return {
+//     title: result.album,
+//     description: result.text,
+//   };
+// }
