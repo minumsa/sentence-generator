38f451863c2ebd3245eb784fe1a8606ba4a24b36
유일 <83910706+minumsa@users.noreply.github.com>
2023-12-11 11:59:34 +0900

    generateMetadata test

--- app/music/post/[id]/page.tsx
+++ app/music/post/[id]/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 
-import { PageProps } from "../../modules/data";
+import { AlbumInfo, PageProps, initialAlbumInfo } from "../../modules/data";
 import { Post } from "../../components/Post";
 import { MusicLayout } from "../../components/MusicLayout";
+import { useEffect, useState } from "react";
+import { fetchDataById } from "../../modules/api";
+
+export async function generateMetadata({ params }: PageProps) {
+  const currentId = params.id;
+  const data = await fetchDataById(currentId);
+
+  return {
+    title: data.album,
+    description: data.text,
+    images: [{ url: data.imgUrl }],
+  };
+}
 
 export default function Page({ params }: PageProps) {
   const currentId = params.id;
+  const [data, setData] = useState<AlbumInfo>(initialAlbumInfo);
+
+  useEffect(() => {
+    async function getData() {
+      const result = await fetchDataById(currentId);
+      setData(result);
+    }
+    getData();
+  }, []);
 
   return (
     <MusicLayout>
