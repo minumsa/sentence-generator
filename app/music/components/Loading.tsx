f2517de27fe87521024088137b2cb74cd949de48
유일 <83910706+minumsa@users.noreply.github.com>
2023-12-26 11:19:20 +0900

    크리스마스 텍스트 삭제

--- app/music/components/Loading.tsx
+++ app/music/components/Loading.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import styles from "../music.module.css";
 
 interface LoadingProps {
@@ -6,37 +5,13 @@ interface LoadingProps {
 }
 
 export const Loading = ({ dataLength }: LoadingProps) => {
-  const [daysLeft, setDaysLeft] = useState<number>();
-
-  useEffect(() => {
-    const today = new Date();
-
-    const christmasDate = new Date(today.getFullYear(), 11, 25);
-
-    const timeDiff = christmasDate.getTime() - today.getTime();
-    const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-    setDaysLeft(days);
-  }, []);
-
-  const christmasText = daysLeft ? (
-    <div>크리스마스까지 {daysLeft}일 남았습니다...</div>
-  ) : daysLeft === 1 ? (
-    <div>오늘은 크리스마스 이브입니다! 🎄</div>
-  ) : daysLeft === 0 ? (
-    <div>오늘은 크리스마스입니다! 🎅🏻</div>
-  ) : undefined;
-
   const loadingText = "데이터 로딩 중입니다...";
-
-  const currentText = daysLeft && daysLeft >= 0 && daysLeft < 31 ? christmasText : loadingText;
-
   const noDataText = "일치하는 데이터가 없습니다...";
   const hasNoData = dataLength === 0;
 
   return hasNoData ? (
     <div className={styles["loading"]}>{noDataText}</div>
   ) : (
-    <div className={styles["loading"]}>{currentText}</div>
+    <div className={styles["loading"]}>{loadingText}</div>
   );
 };
