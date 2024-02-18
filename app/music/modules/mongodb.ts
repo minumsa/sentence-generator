9d49aa6e180d6470450f7e7ab474d17289873e33
minumsa <icycyi@naver.com>
2024-02-18 19:10:54 +0900

    mongodb.ts 코드 수정

--- app/music/modules/mongodb.ts
+++ app/music/modules/mongodb.ts
@@ -1,13 +1,52 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = async () => {
-  try {
-    require("dotenv").config();
-    await mongoose.connect(process.env.MONGODB_URI!);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error(error);
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
+
+function getMongoDBUri(): string {
+  const MONGODB_URI = process.env.MONGODB_URI!;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local or .env.test"
+    );
   }
-};
+  return MONGODB_URI;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectMongoDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(getMongoDBUri(), opts).then(mongoose => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  mongoose.set("bufferCommands", false);
+
+  return cached.conn;
+}
 
 export default connectMongoDB;
+
+export const disconnectMongoDB = async () => {
+  await mongoose.disconnect();
+};
