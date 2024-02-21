0190e18ed2b89da80a0e7aa1268b39866e1999b6
유일 <83910706+minumsa@users.noreply.github.com>
2024-02-21 12:02:45 +0900

    수정 페이지 인터페이스 수정

--- app/music/components/Update.tsx
+++ app/music/components/Update.tsx
@@ -47,8 +47,6 @@ export default function Update({ currentId }: UpdateProps) {
   const [isTyping, setIsTyping] = useState(false);
   const router = useRouter();
 
-  console.log(videos);
-
   // 수정 API
   const handleUpdate = async () => {
     const newAlbumData = await fetchSpotify({
@@ -136,120 +134,133 @@ export default function Update({ currentId }: UpdateProps) {
     >
       <div className={styles["upload-container"]}>
         <div className={styles["title"]}>수정 페이지</div>
-        <div className={styles["upload-item-title"]}>장르</div>
-        <div className={styles["select-container"]}>
-          <select
-            className={styles["select"]}
-            value={genre}
-            onChange={e => {
-              setGenre(e.target.value);
-            }}
-          >
-            <option value="">--장르를 선택해주세요--</option>
-            {Object.entries(contents).map(([key, value]) => {
-              return (
-                <option value={key} key={key}>
-                  {value}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <div className={styles["upload-item-title"]}>링크(Apple Music)</div>
-        <textarea
-          className={styles["input"]}
-          value={link}
-          onChange={e => {
-            setLink(e.target.value);
-          }}
-        />
-        <div className={styles["upload-item-title"]}>앨범 제목</div>
-        <div>
-          <input
-            className={styles["input"]}
-            value={albumKeyword}
-            onChange={e => {
-              setAlbumKeyword(e.target.value);
-              setIsTyping(true);
-            }}
-            placeholder="검색어를 입력해주세요"
-          />
-          <div
-            className={styles["search-album-modal-container"]}
-            style={{ display: albumKeyword && searchData ? "flex" : "none" }}
-          >
-            {searchData?.map((data, index) => {
-              const artist = data.artists[0].name;
-              const album = data.name;
-              const releaseYear = data.release_date.slice(0, 4);
-              const imageUrl = data.images[2].url;
-              return (
-                <div
-                  className={styles["search-album-modal"]}
-                  key={index}
-                  onClick={() => {
-                    handleModal(data);
-                  }}
-                >
-                  <div className={styles["search-album-image-container"]}>
-                    <img
-                      className={styles["search-album-image"]}
-                      src={imageUrl}
-                      alt="search-album-image"
-                      loading="lazy"
-                    />
-                  </div>
-                  <div className={styles["search-album-text"]}>
-                    <div>
-                      <span>{album}</span>
-                      <span style={{ paddingLeft: "5px", color: "#757A84" }}>({releaseYear})</span>
-                    </div>
-                    <div style={{ fontWeight: 400 }}>{artist}</div>
-                  </div>
-                </div>
-              );
-            })}
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>장르</div>
+          <div className={styles["select-container"]}>
+            <select
+              className={styles["select"]}
+              value={genre}
+              onChange={e => {
+                setGenre(e.target.value);
+              }}
+            >
+              <option value="">--장르를 선택해주세요--</option>
+              {Object.entries(contents).map(([key, value]) => {
+                return (
+                  <option value={key} key={key}>
+                    {value}
+                  </option>
+                );
+              })}
+            </select>
           </div>
         </div>
-        <div className={styles["upload-item-title"]}>앨범 ID(Spotify)</div>
-        <textarea
-          className={styles["input"]}
-          value={albumId}
-          onChange={e => {
-            setAlbumId(e.target.value);
-          }}
-        />
-        <div className={styles["upload-item-title"]}>아티스트 ID(Spotify)</div>
-        <textarea
-          className={styles["input"]}
-          value={artistId}
-          onChange={e => {
-            setArtistId(e.target.value);
-          }}
-        />
-        <div className={styles["upload-item-title"]}>별점</div>
-        <div className={styles["select-container"]}>
-          <select
-            className={styles["select"]}
-            value={score}
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>링크(Apple Music)</div>
+          <input
+            className={styles["input"]}
+            value={link}
             onChange={e => {
-              setScore(Number(e.target.value));
+              setLink(e.target.value);
             }}
-          >
-            <option value="">--스코어를 선택해주세요--</option>
-            {scoreArray.map((item, index) => {
-              return <option key={index}>{item}</option>;
-            })}
-          </select>
+          />
         </div>
-        <div className={styles["upload-item-title"]}>글</div>
-        <textarea
-          className={`${styles["input"]} ${styles["input-text"]}`}
-          value={text}
-          onChange={e => {
-            setText(e.target.value);
-          }}
-        />
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>앨범 제목</div>
+          <div>
+            <input
+              className={styles["input"]}
+              value={albumKeyword}
+              onChange={e => {
+                setAlbumKeyword(e.target.value);
+                setIsTyping(true);
+              }}
+              placeholder="검색어를 입력해주세요"
+            />
+            <div
+              className={styles["search-album-modal-container"]}
+              style={{ display: albumKeyword && searchData ? "flex" : "none" }}
+            >
+              {searchData?.map((data, index) => {
+                const artist = data.artists[0].name;
+                const album = data.name;
+                const releaseYear = data.release_date.slice(0, 4);
+                const imageUrl = data.images[2].url;
+                return (
+                  <div
+                    className={styles["search-album-modal"]}
+                    key={index}
+                    onClick={() => {
+                      handleModal(data);
+                    }}
+                  >
+                    <div className={styles["search-album-image-container"]}>
+                      <img
+                        className={styles["search-album-image"]}
+                        src={imageUrl}
+                        alt="search-album-image"
+                        loading="lazy"
+                      />
+                    </div>
+                    <div className={styles["search-album-text"]}>
+                      <div>
+                        <span>{album}</span>
+                        <span style={{ paddingLeft: "5px", color: "#757A84" }}>
+                          ({releaseYear})
+                        </span>
+                      </div>
+                      <div style={{ fontWeight: 400 }}>{artist}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles["upload-item-title"]}>앨범 ID(Spotify)</div>
+          <input
+            className={styles["input"]}
+            value={albumId}
+            onChange={e => {
+              setAlbumId(e.target.value);
+            }}
+          />
+          <div className={styles["upload-item-title"]}>아티스트 ID(Spotify)</div>
+          <input
+            className={styles["input"]}
+            value={artistId}
+            onChange={e => {
+              setArtistId(e.target.value);
+            }}
+          />
+        </div>
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>별점</div>
+          <div className={styles["select-container"]}>
+            <select
+              className={styles["select"]}
+              value={score}
+              onChange={e => {
+                setScore(Number(e.target.value));
+              }}
+            >
+              <option value="">--스코어를 선택해주세요--</option>
+              {scoreArray.map((item, index) => {
+                return <option key={index}>{item}</option>;
+              })}
+            </select>
+          </div>
+        </div>
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>글</div>
+          <textarea
+            className={`${styles["input"]} ${styles["input-text"]}`}
+            value={text}
+            onChange={e => {
+              setText(e.target.value);
+            }}
+            style={{ textAlign: "start" }}
+          />
+        </div>
         {data &&
           new Array(videoCount).fill(null).map((_, index) => {
             const tmpVideos = [...videos];
@@ -383,15 +394,17 @@ export default function Update({ currentId }: UpdateProps) {
                     setVideos(tmpVideos);
                   }}
                 />
-                <div className={styles["upload-item-title"]}>{`영상 링크 ${videoNumber}`}</div>
-                <input
-                  className={`${styles["input"]} ${styles["input-link"]}`}
-                  value={videos[index].url}
-                  onChange={e => {
-                    tmpVideos[index] = { ...tmpVideos[index], url: e.target.value };
-                    setVideos(tmpVideos);
-                  }}
-                />
+                <div className={styles["upload-item-container"]}>
+                  <div className={styles["upload-item-title"]}>{`영상 링크 ${videoNumber}`}</div>
+                  <input
+                    className={`${styles["input"]} ${styles["input-link"]}`}
+                    value={videos[index].url}
+                    onChange={e => {
+                      tmpVideos[index] = { ...tmpVideos[index], url: e.target.value };
+                      setVideos(tmpVideos);
+                    }}
+                  />
+                </div>
               </div>
             );
           })}
@@ -411,26 +424,28 @@ export default function Update({ currentId }: UpdateProps) {
             setMusicVideoUrl(e.target.value);
           }}
         /> */}
-        <div className={styles["upload-item-title"]}>작성일</div>
-        <DatePicker
-          selected={uploadDate}
-          onChange={date => date && setUploadDate(date)}
-          dateFormat={"yyyy/MM/dd"}
-          className={styles["date-input"]}
-        />
-        <div className={styles["upload-item-title"]} style={{ marginTop: "50px" }}>
-          관리자 비밀번호
+        <div className={styles["upload-item-container"]} style={{ paddingBottom: "50px" }}>
+          <div className={styles["upload-item-title"]}>작성일</div>
+          <DatePicker
+            selected={uploadDate}
+            onChange={date => date && setUploadDate(date)}
+            dateFormat={"yyyy/MM/dd"}
+            className={styles["date-input"]}
+          />
         </div>
-        <input
-          className={styles["input"]}
-          value={password}
-          onChange={e => {
-            setPassword(e.target.value);
-          }}
-          onKeyDown={handlePasswordEnter}
-          style={{ width: "245px" }}
-        />
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className={styles["upload-item-container"]}>
+          <div className={styles["upload-item-title"]}>관리자 비밀번호</div>
+          <input
+            className={styles["input"]}
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+            onKeyDown={handlePasswordEnter}
+            style={{ width: "245px" }}
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", padding: "30px 0" }}>
           <div
             className={`${styles["button"]} ${styles["submit"]}`}
             onClick={() => {
