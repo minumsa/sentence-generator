e79a93a7ac1644879a569ef760114b62ed8c97db
minumsa <icycyi@naver.com>
2024-03-22 15:44:14 +0900

    Test

--- app/music/admin/page.tsx
+++ app/music/admin/page.tsx
@@ -4,13 +4,13 @@ import { PER_PAGE_COUNT } from "../modules/constants";
 
 export default async function Page() {
   try {
-    const scrollCount = 1;
-    const pathName = "";
-    const currentTagKey = "";
+    // const scrollCount = 1;
+    // const pathName = "";
+    // const currentTagKey = "";
 
-    const queryString = `?pathName=${pathName}&currentPage=${scrollCount}&currentTagKey=${currentTagKey}`;
-    const url = `https://divdivdiv.com/music/api${queryString}`;
-    // const url = `https://divdivdiv.com/music/api`;
+    // const queryString = `?pathName=${pathName}&currentPage=${scrollCount}&currentTagKey=${currentTagKey}`;
+    // const url = `https://divdivdiv.com/music/api${queryString}`;
+    const url = `https://divdivdiv.com/music/api`;
 
     const response = await fetch(url, {
       method: "GET",
