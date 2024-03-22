6339a649b4f65817c8f9dfb8a7627a1fbc40e718
minumsa <icycyi@naver.com>
2024-03-22 14:45:20 +0900

    장르 api 따로 만들기

--- app/music/[genre]/page.tsx
+++ app/music/[genre]/page.tsx
@@ -2,16 +2,14 @@ import Content from "../components/@common/Content";
 import { MusicLayout } from "../components/@common/MusicLayout";
 import { PageProps } from "../modules/types";
 
+const CURRENT_PAGE = 1;
+
 export default async function Page({ params }: PageProps) {
   let currentGenre = params.genre;
-  const currentPage = 1;
 
   try {
-    const pathName = currentGenre;
-    const currentTagKey = "";
-
-    const queryString = `?pathName=${pathName}&currentPage=${currentPage}&currentTagKey=${currentTagKey}`;
-    const url = `https://divdivdiv.com/music/api${queryString}`;
+    const queryString = `?currentGenre=${currentGenre}&currentPage=${CURRENT_PAGE}`;
+    const url = `https://divdivdiv.com/music/api/genre${queryString}`;
 
     const response = await fetch(url, {
       method: "GET",
@@ -29,7 +27,7 @@ export default async function Page({ params }: PageProps) {
 
     return (
       <MusicLayout>
-        <Content data={slicedData} totalDataLength={totalDataLength} currentPage={currentPage} />
+        <Content data={slicedData} totalDataLength={totalDataLength} currentPage={CURRENT_PAGE} />
       </MusicLayout>
     );
   } catch (error) {
---
+++ app/music/api/genre/route.ts
@@ -0,0 +1,39 @@
+import connectMongoDB from "@/app/music/modules/mongodb";
+import Music from "@/models/music";
+import { NextResponse } from "next/server";
+import { SortOrder } from "mongoose";
+import { SUB_PER_PAGE_COUNT } from "../../modules/constants";
+
+export async function GET(request: Request) {
+  try {
+    await connectMongoDB();
+
+    const url = new URL(request.url);
+    const currentPage = Number(url.searchParams.get("currentPage"));
+    const currentGenre = url.searchParams.get("currentGenre") ?? "";
+
+    interface SortKey {
+      [key: string]: SortOrder;
+    }
+
+    const sortKey: SortKey = { score: -1, artist: 1 };
+
+    interface Query {
+      genre: string;
+    }
+
+    let query: Query = { genre: currentGenre };
+
+    const skipCount = SUB_PER_PAGE_COUNT * currentPage - SUB_PER_PAGE_COUNT;
+    const slicedData =
+      currentPage === 1
+        ? await Music.find(query).sort(sortKey).limit(SUB_PER_PAGE_COUNT)
+        : await Music.find(query).sort(sortKey).skip(skipCount).limit(SUB_PER_PAGE_COUNT);
+    const totalDataLength = await Music.find(query).count();
+
+    return NextResponse.json({ slicedData, totalDataLength });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Server Error" }, { status: 500 });
+  }
+}
