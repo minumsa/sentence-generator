5ec2737e83f7f71420155bd95b9f339a5c99f0b2
minumsa <icycyi@naver.com>
2024-03-25 17:40:51 +0900

    카버차트 프로젝트 삭제

--- app/music/components/post/assets/LinkIcon.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./LinkIcon.module.css";
-
-export const LinkIcon = () => {
-  return (
-    <span>
-      <img className={styles["link-icon"]} src="/music/link.svg" alt="link-icon" />
-    </span>
-  );
-};
--- app/music/modules/atoms.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-import { AlbumInfo } from "./types";
-
-export const scrollCountAtom = atom<number>(1);
-export const scrollPositionAtom = atom<number>(0);
-export const currentTotalScrollCountAtom = atom<number>(0);
-export const albumDataAtom = atom<AlbumInfo[]>([]);
-export const CurrentTagKeyAtom = atom<string>("");
